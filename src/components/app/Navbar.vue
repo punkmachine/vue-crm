952ee70cf3ca0ca183550d8e19872177d0a3d4ce
punkmachine <alex_rassydihin@mail.ru>
2022-04-02 14:15:58 +0600

    урок №4.

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,23 +5,27 @@
 				<a href="#" @click.prevent="$emit('click')">
 					<i class="material-icons black-text">dehaze</i>
 				</a>
-				<span class="black-text">12.12.12</span>
+				<span class="black-text">{{ date | date('datetime') }}</span>
 			</div>
 
 			<ul class="right hide-on-small-and-down">
 				<li>
-					<a class="dropdown-trigger black-text" href="#" data-target="dropdown">
+					<a class="dropdown-trigger black-text" data-target="dropdown" ref="dropdown">
 						USER NAME
 						<i class="material-icons right">arrow_drop_down</i>
 					</a>
 
 					<ul id="dropdown" class="dropdown-content">
 						<li>
-							<a href="#" class="black-text"> <i class="material-icons">account_circle</i>Профиль </a>
+							<router-link to="/profile" class="black-text">
+								<i class="material-icons">account_circle</i>Профиль
+							</router-link>
 						</li>
 						<li class="divider" tabindex="-1"></li>
 						<li>
-							<a href="#" class="black-text"> <i class="material-icons">assignment_return</i>Выйти </a>
+							<a href="#" class="black-text" @click.prevent="exitClick">
+								<i class="material-icons">assignment_return</i>Выйти
+							</a>
 						</li>
 					</ul>
 				</li>
@@ -31,7 +35,34 @@
 </template>
 
 <script>
+import M from 'materialize-css';
+
 export default {
 	name: 'app-navbar',
+	data() {
+		return {
+			date: new Date(),
+			interval: null,
+			dropdown: null,
+		};
+	},
+	methods: {
+		exitClick() {
+			this.$router.push('/login?message=exit');
+		},
+	},
+	mounted() {
+		this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+			constrainWidth: true,
+		});
+
+		this.interval = setInterval(() => {
+			this.date = new Date();
+		}, 1000);
+	},
+	beforeDestroy() {
+		clearInterval(this.interval);
+		this.dropdown?.destroy();
+	},
 };
 </script>
